cbf084badd13c051b9b2beb6a35ec60170bbd600
Eduarda <eduarda@houpa.app>
2023-08-09 22:58:25 -0300

    Add index and create of supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-        return view('admin/supports/index');
+    public function index(Support $support)
+    {
+
+        $supports = $support->all();
+        return view('admin/supports/index' , compact($supports));
+    }
+
+    public function create()
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(Request $request)
+    {
+        echo 'Cadastrar';
+        dd($request->all());
+
     }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,20 @@
 <h1>Listagem dos chamados</h1>
+<a href="{{route('supports.create')}}">Criar chamado </a>
+<table>
+    <thead>
+        <th>assuntos</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach($supports as $support)
+            <tr>
+                <td>{{$support->subject}}</td>
+                <td>{{$support->status}}</td>
+                <td>{{$support->body}}</td>
+                <td> > </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
